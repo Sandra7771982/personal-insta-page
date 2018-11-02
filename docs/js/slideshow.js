81e841541c2d171c00ff90ae2314cbc409209c19
Sandra <aleks-fine@yandex.ru>
2018-11-02 18:02:24 +0300

    Второе д/з
    1. изменен фоновый градиент
    2. изменен шрифт имени
    3. написан слайдер

---
+++ docs/js/slideshow.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+
+    let slideIndex = 0;
+    showPhotos();
+
+    function showPhotos() {
+        let i;
+        let slides = $('.photo img');
+        for (i = 0; i < slides.length; i++) {
+            $(slides[i]).hide();
+        }
+
+        slideIndex++;
+
+        if (slideIndex > slides.length) {
+            slideIndex = 1;
+        }
+
+        $(slides[slideIndex-1]).fadeIn();
+
+        setTimeout(showPhotos, 2000);
+
+    }
+
+});
