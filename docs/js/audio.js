d920b2e2e7b720e68f4b9b921fed3e282f0ecaa3
Sandra <aleks-fine@yandex.ru>
2018-11-04 22:46:31 +0300

    Четвертое д/з
    написан скрипт для музыки
    подправлен html
    поменян цвет фона
    добавлен параллакс для атмосферности

--- docs/js/audio.js
+++ docs/js/audio.js
@@ -1,5 +1,33 @@
 $(document).ready(function(){
 
+    let audio = $('#audioTrack')[0];
+    console.log(audio);
 
+    $playBtn = $('#audio-play');
+    $pauseBtn = $('#audio-pause');
+    $audioControl = $('[data-type="audio-control"]');
+
+
+
+    initAudioPlayer();
+    function initAudioPlayer() {
+        $playBtn.parent().show();
+        audio.loop = true;
+
+        $audioControl.click(function () {
+            if(audio.paused) {
+                audio.play();
+                $playBtn.parent().hide();
+                $pauseBtn.parent().show();
+            } else {
+                audio.pause();
+                $playBtn.parent().show();
+                $pauseBtn.parent().hide();
+            }
+        });
+    }
+
+
+    // audio.play();
 
 });
